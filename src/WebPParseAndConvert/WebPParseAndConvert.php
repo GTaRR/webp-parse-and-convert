1443ca90569fbc8858b6b7680479c3fc989a59d0
Vladimir Sushilov <33573215+GTaRR@users.noreply.github.com>
2019-05-30 13:20:19 +0300

    Create WebPParseAndConvert.php

---
+++ src/WebPParseAndConvert/WebPParseAndConvert.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace WebPParseAndConvert;
+
+use WebPConvert\WebPConvert;
+
+class WebPParseAndConvert
+{
+    private $content;
+    private $rootDir;
+    private $images;
+    private $formats = array(
+        '.jpg',
+        '.jpeg',
+        '.png'
+    );
+    private $patterns = array(
+        array(
+            'pattern' => '<img[^>]+src=("[^"]*")[^>]+>',
+            'exclude' => array('"','./')
+        ),
+        array(
+            'pattern' => '/background-image:.+url\(([^"]+)\)/i',
+            'exclude' => array("'","./")
+        ),
+//        array(
+//            'pattern' => '/background:.+url\(([^"]+)\)/i',
+//            'exclude' => array("'","./")
+//        ),
+//        array(
+//            'pattern' => '/data-src=\"([^"]+)\"/i',
+//            'exclude' => array("'","./")
+//        ),
+    );
+    private $notSupportDevice = array(
+        'iphone',
+        'ipod',
+        'ipad',
+        'macintosh',
+        'mac os',
+        'Edge',
+        'MSIE'
+    );
+
+    /**
+     * @param   string      $content - HTML загружаемой страницы
+     * @param   string      $rootDir - Корень сайта в файловой системе
+     * @param   array       $options - Дополнительные опции
+     * @return  string      &$content
+     */
+    public function __construct($content, $rootDir, $options = array())
+    {
+        if(!isset($content) || empty($content)) return false;
+        $this->content = $content;
+        $this->rootDir = ($rootDir) ? $rootDir : $_SERVER['DOCUMENT_ROOT'];
+        if(isset($options['formats']) && is_array($options['formats']))
+            $this->formats = $options['formats'];
+        if(isset($options['patterns']) && is_array($options['patterns']))
+            $this->patterns = $options['patterns'];
+        if(isset($options['devices']) && is_array($options['devices']))
+            $this->notSupportDevice = $options['devices'];
+    }
+
+    /**
+     * Парсинг изображений на странице
+     *
+     * @param   string      $pattern - Регулярное выражение для поиска изображений
+     * @param   string      $content - HTML загружаемой страницы
+     * @param   array       $exclude - Исключаемые строки
+     * @return  array       $images  - Массив адресов изображений
+     */
+    private function parseImgByPatterns($pattern, $content, $exclude = array())
+    {
+        $images = array();
+        preg_match_all($pattern, $content, $result);
+        if (count($result)) {
+            foreach ($result[1] as $img) {
+                if(is_array($exclude) && count($exclude) > 0) {
+                    foreach ($exclude as $search){
+                        $img = str_replace($search, "", $img);
+                    }
+                }
+                foreach ($this->formats as $format) {
+                    if ((strpos(strtolower($img), $format) !== false))
+                        $images[] = $img;
+                }
+            }
+        }
+        return $images;
+    }
+
+    /**
+     * Конвертирование массива изображений в WebP по адресу [path/name.*].webp
+     * и сохранением исходных файлов [path/name.*]
+     *
+     * @param   string      $content - HTML загружаемой страницы
+     * @param   array       $images  - Массив адресов изображений
+     * @return  string      $content
+     */
+    private function convertImages($content, $images)
+    {
+        foreach ($images as $img_src_rel)
+        {
+            if (($img_src_rel) && (file_exists($this->rootDir . $img_src_rel)))
+            {
+                if (!file_exists($this->rootDir . $img_src_rel . '.webp'))
+                {
+                    $img_src_abs = $this->rootDir . $img_src_rel;
+                    $destination = $this->rootDir . $img_src_rel . '.webp';
+
+                    // во избежании ошибок обработки png картинок с расширениями .jpg/.jpeg
+                    if (!in_array('.png', $this->formats)
+                        && strpos(strtolower($img_src_abs), '.png') === false
+                        && mime_content_type($img_src_abs) === 'image/png') continue;
+
+                    if (WebPConvert::convert(
+                        $img_src_abs,
+                        $destination,
+                        $options = [
+//                                'png' => ['converters' => ['imagick']]
+                        ]
+//                            new \WebPConvert\Loggers\EchoLogger() // Логгер конвертера
+                    )) {
+                        $img_dest = $img_src_rel . '.webp';
+                    } else {
+                        $img_dest = $img_src_rel;
+                    }
+                } else {
+                    $img_dest = $img_src_rel . '.webp';
+                }
+
+                $content = str_replace($img_src_rel, $img_dest, $content);
+            }
+        }
+        return $content;
+    }
+
+    /**
+     * @return  string      $content - Итоговый контент страницы
+     */
+    public function execute()
+    {
+        if(!isset($this->content) || empty($this->content)) return false;
+
+        $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+        foreach ($this->notSupportDevice as $val) {
+            if (stripos($userAgent, $val) !== false) return false;
+        }
+
+        $this->images = array();
+
+        if(count($this->patterns) > 0) {
+            foreach ($this->patterns as $pattern) {
+                $this->images = array_merge(
+                    $this->images,
+                    $this->parseImgByPatterns($pattern['pattern'], $this->content, $pattern['exclude'])
+                );
+            }
+        }
+
+        $this->images = array_unique($this->images);
+
+        if (count($this->images))
+            return $this->convertImages($this->content, $this->images);
+        else
+            return false;
+    }
+}
