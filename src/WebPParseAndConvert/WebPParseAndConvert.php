79ba26e1cc09657e477abdebe1892aa402a57e72
Vladimir Sushilov <33573215+GTaRR@users.noreply.github.com>
2019-05-31 22:11:28 +0300

    Update WebPParseAndConvert.php

--- src/WebPParseAndConvert/WebPParseAndConvert.php
+++ src/WebPParseAndConvert/WebPParseAndConvert.php
@@ -16,7 +16,7 @@ class WebPParseAndConvert
     );
     private $patterns = array(
         array(
-            'pattern' => '<img[^>]+src=("[^"]*")[^>]+>',
+            'pattern' => '/<img[^>]+src=("[^"]*")[^>]+>/i',
             'exclude' => array('"','./')
         ),
         array(
